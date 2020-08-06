a7b93c5ad8990eca4819938ef67fefc297f5c368
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-06 09:07:16 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,8 +13,9 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+                 shipping_manifest["oil paintings"]
+
 end
-shipping_manifest["oil paintings"]
 
 def adding
   shipping_manifest = {
